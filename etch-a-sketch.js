6295cad36cb5d5ed0831807617828366e7c15cf0
Johan <jpettersen50@gmail.com>
2022-06-21 20:41:48 +0200

    :(

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,46 @@
 const container = document.getElementById("container");
+const containerBorder = document.getElementById("containerBorder");
+const gridbox = document.createElement('div');
+const gridboxes = document.getElementsByClassName("gridElement");
 
 
-let xGrid = 5;
-let yGrid = 5;
+let xGrid = 50;
+let yGrid = 50;
 
 
 
 let boxSize = xGrid * yGrid;
 
+function changeContainerSize() {
+    containerBorder.style.width = "" + ((xGrid  * 1.1) + 1) + "vw";
+    containerBorder.style.height = "" + ((yGrid * 1.1) + 1) + "vw";
+    container.style.width = "" + (xGrid  * 1.1) + "vw";
+    container.style.height = "" + (yGrid * 1.1) + "vw";
+
+
+
+
+}
+
+
 function createGridBox() {
-    container.style.width = "" + xGrid + "vw";
-    container.style.height = "" + yGrid + "vw";
-    container.style.gridTemplateColumns = "repeat(" + xGrid + ", 1fr)";
-    container.style.gridTemplateRows= "repeat(" + yGrid + ", 1fr)";
+
+    changeContainerSize();
+    container.style.gridTemplateColumns = "repeat(" + xGrid + ", 1vw)";
+    container.style.gridTemplateRows= "repeat(" + yGrid + ", 1vw)";
 
 
     for(i = 0; i < boxSize; i++) {
-
 
         const gridbox = document.createElement('div');
         gridbox.className="gridElement";
         container.appendChild(gridbox);
 
+        
     }
-
+    
+   
+    
 }
 
 
