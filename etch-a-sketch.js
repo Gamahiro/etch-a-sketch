0b880d044adc0520b4abfb0439425048b6ebbbcc
Johan <jpettersen50@gmail.com>
2022-06-21 17:16:45 +0200

    boilerplate

---
+++ etch-a-sketch.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+
+
+let boxSize = 16 * 16;
+
+function createGridBox() {
+
+    for(i = boxSize; i < boxSize; i++) {
+        
+    }
+
+
+    const gridbox = document.createElement('div');
+    container.appendChild(gridbox);
+    container.appendChild(gridbox);
+
+}
+
+
+createGridBox();
